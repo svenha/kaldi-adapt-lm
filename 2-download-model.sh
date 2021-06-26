1d61724dd2a0de33aa57c0fe2e10a4aff95d241d
Florian Quirin <info@bytemind.de>
2021-06-26 20:17:00 +0200

    Update 2-download-model.sh

--- 2-download-model.sh
+++ 2-download-model.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # Download base model to adapt
-wget https://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-de-tdnn_250-r20190328.tar.xz
-#wget https://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-en-tdnn_250-r20190609.tar.xz
-mkdir model
-tar --strip-components=1 -xf kaldi-generic-de-tdnn_250-r20190328.tar.xz -C model
-#tar --strip-components=1 -xf kaldi-generic-en-tdnn_250-r20190609.tar.xz -C model
+MODEL_LANG="en"
+if [ -n "$1" ]; then
+	MODEL_LANG="$1"
+fi
+if [ "$MODEL_LANG" = "en" ]; then
+	wget https://github.com/fquirin/kaldi-adapt-lm/releases/download/v1.0.0/kaldi-generic-en-tdnn_250.zip
+	unzip kaldi-generic-en-tdnn_250.zip -d model
+elif [ "$MODEL_LANG" = "de" ]; then
+	wget https://github.com/fquirin/kaldi-adapt-lm/releases/download/v1.0.0/kaldi-generic-de-tdnn_250.zip
+	unzip kaldi-generic-de-tdnn_250.zip -d model
+elif [ "$MODEL_LANG" = "de_zamia_org" ]; then
+	wget https://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-de-tdnn_250-r20190328.tar.xz
+	mkdir model && tar --strip-components=1 -xf kaldi-generic-de-tdnn_250-r20190328.tar.xz -C model
+elif [ "$MODEL_LANG" = "en_zamia_org" ]; then
+	wget https://goofy.zamia.org/zamia-speech/asr-models/kaldi-generic-en-tdnn_250-r20190609.tar.xz
+	mkdir model && tar --strip-components=1 -xf kaldi-generic-en-tdnn_250-r20190609.tar.xz -C model
+else
+	echo "Unknown argument. Use one of: 'en', 'de', 'en_zamia_org', 'de_zamia_org'"
+fi
